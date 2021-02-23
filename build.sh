080224f4b16b309e68b1b962a97eedc35b9ed1ee
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2021-02-23 12:01:44 +0330

    WIP

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-docker build -t liararepo/php-platform .
+docker build -t liararepo/php-platform:php7.2 -f Dockerfile.php7.2 .
+docker build -t liararepo/php-platform:php7.3 -f Dockerfile.php7.3 .
+docker build -t liararepo/php-platform:php7.4 -f Dockerfile.php7.4 .
