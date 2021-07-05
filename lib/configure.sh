308505ab01f73709b56c20221b9a44ed159a3f9c
eng-tehrani <haghft@gmail.com>
2021-07-05 14:21:18 +0430

    fix composer.lock bug

--- lib/configure.sh
+++ lib/configure.sh
@@ -2,8 +2,8 @@
 
 if [ "$__PHP_MIRROR" = "true" ]; then
   echo '> Using mirror: ' $__PHP_MIRRORURL
-  composer config -g repo.packagist false
-  composer config -g repo.liara composer $__PHP_MIRRORURL
+  composer config repo.packagist false
+  composer config repo.liara composer $__PHP_MIRRORURL
 fi
 
 composer update --lock --no-dev --no-interaction --prefer-dist --optimize-autoloader --ansi --no-scripts
